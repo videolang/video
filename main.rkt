6ba295a45790f142f081fde28eda33a86876947b
Leif Andersen <leif@leifandersen.net>
2016-10-10 23:41:19 -0400

    Add declarative structure for video

--- main.rkt
+++ main.rkt
@@ -1,35 +1,134 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require racket/match
+         "private/mlt.rkt")
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+;; Calls mlt-*-service on the correct data type
+;;    (getting the service type)
+;; Service -> _mlt-service
+(define (mlt-*-service video-object)
+  (cond
+    [(link? video-object)
+     (mlt-*-service (link-target video-object))]
+    [(producer? video-object)
+     (mlt-producer-service (video-mlt-object video-object))]
+    [(filter? video-object)
+     (mlt-filter-service (video-mlt-object video-object))]
+    [else (error 'video "Unsupported video: ~a" video-object)]))
 
-;; Code here
+;; Connect target to source
+;; Video-Object _mlt-service Integer -> _ibool
+(define (mlt-*-connect target source-service [index #f])
+  (cond
+    [(consumer? target)
+     (mlt-consumer-connect (video-mlt-object target)
+                           source-service)]
+    [(filter? target)
+     (mlt-filter-connect (video-mlt-object target)
+                         source-service
+                         index)]
+    [else (error 'video "Unsupported target ~a" target)]))
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+;; Convert a video object into an MLT object
+;; Video-Object _mlt-profile -> MLT-Object
+(define (convert-to-mlt! data p)
+  (define ret
+    (match data
+      [(struct* link ([source source]
+                      [target target]
+                      [index index]))
+       (define source* (convert-to-mlt! source p))
+       (define target* (convert-to-mlt! target p))
+       (mlt-*-connect target (mlt-*-service source) index)
+       target*]
+      [(struct* clip ([source source]))
+       (define type (producer-type data))
+       (mlt-factory-producer p type source)]
+      [(struct* consumer ([type type]
+                          [target target]))
+       (mlt-factory-consumer type target)]
+      [(struct* filter ([type type]))
+       (mlt-factory-filter p type #f)] ;; TODO, should probably not be #f?
+      [_ (error 'video "Unsuported data ~a" data)]))
+  (when (video? data)
+    (set-video-mlt-object! data ret))
+  ret)
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+(define (render data)
+  
+  (unless (mlt-factory-init #f)
+    (error "Unable to locate factory modules"))
+  (define p (mlt-profile-init #f))
+  
+  (define target (convert-to-mlt! data p))
+   
+  (mlt-consumer-start target)
+  
+  (let loop ()
+    (sleep 1)
+    (unless (mlt-consumer-is-stopped target)
+      (loop))))
+
+(struct video ([mlt-object #:mutable]))
+(struct link video (source target index))
+(struct properties video (prop))
+(struct frame properties ())
+(struct service properties (filters))
+(struct filter service (type))
+(struct transition service (type))
+(struct consumer service (type target))
+(struct producer service (type))
+(struct playlist producer (producers))
+(struct clip producer (source start end))
+(struct tractor producer (inputs output))
+(struct multitrack producer (tracks))
+(struct field producer (filters/transitions))
+
+(define (build-video-object class-type
+                            #:source [source #f]
+                            #:target [target #f]
+                            #:index [index 0]
+                            #:properties [prop (hash)]
+                            #:mlt-object [mlt-object #f]
+                            #:filters [filters '()]
+                            #:type [type #f]
+                            #:producers [producers '()]
+                            #:start [start #f]
+                            #:end [end #f]
+                            #:inputs [inputs '()]
+                            #:output [output #f]
+                            #:tracks [tracks '()]
+                            #:filters/transitions [f/t '()])
+  (match class-type
+    ['video (video mlt-object)]
+    ['link (link mlt-object source target index)]
+    ['properties (properties mlt-object prop)]
+    ['frame (frame mlt-object prop)]
+    ['service (service mlt-object prop filters)]
+    ['filter (filter mlt-object prop filters type)]
+    ['transition (transition mlt-object prop filters type)]
+    ['consumer (consumer mlt-object prop filters type target)]
+    ['producer (producer mlt-object prop filters type)]
+    ['playlist (playlist mlt-object prop filters type producers)]
+    ['clip (clip mlt-object prop filters type source start end)]
+    ['tractor (tractor mlt-object prop filters type inputs output)]
+    ['multitrack (multitrack mlt-object prop filters type tracks)]
+    ['field (field mlt-object prop filters type f/t)]
+    [_ (error 'video "Invalid type ~a" class-type)]))
+
+#;
+(render
+ (build-video-object
+  'link
+  #:source (build-video-object 'clip #:source "/Users/leif/demo.mkv")
+  #:target (build-video-object 'consumer)))
+
+(render
+ (build-video-object
+  'link
+  #:source (build-video-object
+            'link
+            #:source (build-video-object 'clip #:source "/Users/leif/demo.mkv")
+            #:target (build-video-object 'filter #:type 'grayscale)
+            #:index 0)
+  #:target (build-video-object 'consumer)))
---
+++ private/mlt.rkt
@@ -0,0 +1,118 @@
+#lang racket/base
+
+(require ffi/unsafe
+         ffi/unsafe/define
+         racket/gui/base)
+(provide (all-defined-out))
+
+(define mlt-lib (ffi-lib "libmlt" '("6")))
+(define-ffi-definer define-mlt mlt-lib)
+
+;; Types
+(define _ibool (make-ctype _bool #f not))
+(define _symbol-or-null
+  (make-ctype _string
+              (λ (v) (if (symbol? v) (symbol->string v) v))
+              (λ (v) (if (string? v) (string->symbol v) v))))
+(define-cpointer-type _mlt-repository)
+(define-cpointer-type _mlt-profile)
+(define-cpointer-type _mlt-producer)
+(define-cpointer-type _mlt-consumer)
+(define-cpointer-type _mlt-service)
+(define-cpointer-type _mlt-playlist)
+(define-cpointer-type _mlt-properties)
+(define-cpointer-type _mlt-filter)
+(define-cpointer-type _mlt-transition)
+(define-cpointer-type _mlt-frame)
+(define-cpointer-type _mlt-tractor)
+(define-cpointer-type _mlt-field)
+(define-cpointer-type _mlt-multitrack)
+(define _mlt-position _int32)
+
+;; Factory
+(define-mlt mlt-factory-init (_fun _path -> _mlt-repository/null)
+  #:c-id mlt_factory_init)
+(define-mlt mlt-factory-producer (_fun _mlt-profile _symbol-or-null _string -> _mlt-producer/null)
+  #:c-id mlt_factory_producer)
+(define-mlt mlt-factory-consumer (_fun _mlt-profile/null _symbol-or-null -> _mlt-consumer/null)
+  #:c-id mlt_factory_consumer)
+(define-mlt mlt-factory-filter (_fun _mlt-profile _symbol-or-null _string -> _mlt-filter/null)
+  #:c-id mlt_factory_filter)
+(define-mlt mlt-factory-transition (_fun _mlt-profile _symbol-or-null _string -> _mlt-transition/null)
+  #:c-id mlt_factory_transition)
+
+;; Profile
+(define-mlt mlt-profile-init (_fun _string -> _mlt-profile/null)
+  #:c-id mlt_profile_init)
+
+;; Consumer
+(define-mlt mlt-consumer-connect (_fun _mlt-consumer _mlt-service ->
+                                       (make-ctype
+                                        _int #f
+                                        (λ (v)
+                                          (cond
+                                            [(= 0 v) 'success]
+                                            [(= 1 v) 'error/producer-does-not-accept-input]
+                                            [(= 2 v) 'error/producer-invalid]
+                                            [(= 3 v) 'error/producer-already-registered]
+                                            [else    'warning]))))
+  #:c-id mlt_consumer_connect)
+(define-mlt mlt-consumer-start (_fun _mlt-consumer -> _ibool)
+  #:c-id mlt_consumer_start)
+(define-mlt mlt-consumer-stop (_fun _mlt-consumer -> _ibool)
+  #:c-id mlt_consumer_stop)
+(define-mlt mlt-consumer-is-stopped (_fun _mlt-consumer -> _bool)
+  #:c-id mlt_consumer_is_stopped)
+
+;; Producer
+(define-mlt mlt-producer-close (_fun _mlt-producer -> _void)
+  #:c-id mlt_producer_close)
+(define-mlt mlt-producer-service (_fun _mlt-producer -> _mlt-service)
+  #:c-id mlt_producer_service)
+(define-mlt mlt-producer-optimise (_fun _mlt-producer -> _ibool)
+  #:c-id mlt_producer_optimise)
+
+;; Playlist
+(define-mlt mlt-playlist-init (_fun -> _mlt-playlist/null)
+  #:c-id mlt_playlist_init)
+(define-mlt mlt-playlist-append (_fun _mlt-playlist _mlt-producer -> _ibool)
+  #:c-id mlt_playlist_append)
+(define-mlt mlt-playlist-append-io (_fun _mlt-playlist _mlt-producer _mlt-position _mlt-position ->
+                                         _ibool)
+  #:c-id mlt_playlist_append_io)
+(define-mlt mlt-playlist-close (_fun _mlt-playlist -> _void)
+  #:c-id mlt_playlist_close)
+(define-mlt mlt-playlist-properties (_fun _mlt-playlist -> _mlt-properties)
+  #:c-id mlt_playlist_properties)
+(define-mlt mlt-playlist-producer (_fun _mlt-playlist -> _mlt-producer)
+  #:c-id mlt_playlist_producer)
+(define-mlt mlt-playlist-blank (_fun _mlt-playlist _mlt-position -> _ibool)
+  #:c-id mlt_playlist_blank)
+(define-mlt mlt-playlist-count (_fun _mlt-playlist -> _int)
+  #:c-id mlt_playlist_count)
+
+;; Tractor
+(define-mlt mlt-tractor-new (_fun -> _mlt-tractor/null)
+  #:c-id mlt_tractor_new)
+(define-mlt mlt-tractor-field (_fun _mlt-tractor -> _mlt-field/null)
+  #:c-id mlt_tractor_field)
+(define-mlt mlt-tractor-multitrack (_fun _mlt-tractor -> _mlt-multitrack)
+  #:c-id mlt_tractor_multitrack)
+
+;; MultiTrack
+(define-mlt mlt-multitrack-connect (_fun _mlt-multitrack _mlt-producer -> _ibool)
+  #:c-id mlt_multitrack_connect)
+
+;; Properties
+(define-mlt mlt-properties-set (_fun _mlt-properties _string _string -> _ibool)
+  #:c-id mlt_properties_set)
+(define-mlt mlt-properties-set-int (_fun _mlt-properties _string _int -> _ibool)
+  #:c-id mlt_properties_set_int)
+(define-mlt mlt-properties-set-position (_fun _mlt-properties _string _mlt-position -> _ibool)
+  #:c-id mlt_properties_set_position)
+
+;; Filters
+(define-mlt mlt-filter-connect (_fun _mlt-filter _mlt-service _int -> _ibool)
+  #:c-id mlt_filter_connect)
+(define-mlt mlt-filter-service (_fun _mlt-filter -> _mlt-service)
+  #:c-id mlt_filter_service)
