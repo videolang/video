fa2dd6bf2e07702e4e7eaffd93ab66eebcedcf29
Leif Andersen <leif@leifandersen.net>
2016-10-17 17:52:27 -0400

    Update examples

--- private/examples.rkt
+++ private/examples.rkt
@@ -3,65 +3,56 @@
 ;; This file contains a lot of examples. Nominally these should be check-equal?
 ;; unit tests, but I am not yet sure how best to do that with videos
 
-(require "../main.rkt"
+;; Uncomment the test you wish to use
+
+(require "../render.rkt"
          "video.rkt")
 
+;; Some constants that rely on files in my hard drive
+;; change them for your own tests
 (define demo "/Users/leif/demo.mkv")
 (define logo "/Users/leif/logo.png")
 (define hold:logo "hold:/Users/leif/logo.png")
+(define pixbuf:logo "pixbuf:/Users/leif/logo.png")
 (define water "/Users/leif/water.txt")
 
+;; A simple HELLO world MLT program that renders the demo file
 #;
 (render
  (make-link #:source (make-producer #:source demo)
             #:target (make-consumer)))
 
+;; Simple example of a more complex setup, with a video
+;;  Passed into a grayscale filter, and then played
 #;
 (render
  (make-link
   #:source (make-link
-            #:source (make-producer #:source "/Users/leif/demo.mkv")
+            #:source (make-producer #:source demo)
             #:target (make-filter #:type 'grayscale)
             #:index 0)
   #:target (make-consumer)))
 
+;; Now the filter is attached to the clip.
+;;   Also, introduce playlists.
+;;   Should start colored and then turn to grayscale
 #;
 (render
  (make-link
   #:source (make-playlist
             #:elements (list
-                                 (make-producer
-                                  #:source "/Users/leif/demo.mkv"
-                                  #:start 0
-                                  #:end 100)
-                                 (make-producer
-                                  #:source "/Users/leif/demo.mkv"
-                                  #:filters (list (make-filter #:type 'grayscale))
-                                  #:start 100
-                                  #:end 200)))
+                        (make-producer
+                         #:source demo
+                         #:start 0
+                         #:end 100)
+                        (make-producer
+                         #:source demo
+                         #:filters (list (make-filter #:type 'grayscale))
+                         #:start 100
+                         #:end 200)))
   #:target (make-consumer)))
 
-#;
-(render
- (make-link
-  #:source (make-transition
-            #:playlist (make-playlist
-                        #:producers (list
-                                     (make-producer
-                                      #:source "/Users/leif/demo.mkv"
-                                      #:start 0
-                                      #:end 300)
-                                     (make-producer
-                                      #:source "/Users/leif/demo.mkv"
-                                      #:filters (list (make-filter #:type 'grayscale))
-                                      #:start 200
-                                      #:end 600)))
-            #:type 'luma
-            #:index 0
-            #:length 100)
-  ;#:target (make-consumer #:type 'avformat #:target "output.mp4")))
-  #:target (make-consumer)))
-
+;; Another playlist demo, this time using 3 filters
 #;
 (render
  (make-link
@@ -92,9 +83,9 @@
                  #:filters (list (make-filter #:type 'invert)))
                 #:start 500
                 #:end 900)))
-  ;#:target (make-consumer #:type 'avformat #:target "output.mp4")))
   #:target (make-consumer)))
 
+; Draw a green and then blue square to a file output.mp4
 #;
 (render
  (make-link
@@ -102,54 +93,35 @@
             #:producers (list (make-producer #:type 'color #:source "green" #:start 0 #:end 200)
                               (make-producer #:type 'color #:source "blue" #:start 0 #:end 200)))
   #:target (make-consumer #:type 'avformat #:target "output.mp4")))
-  ;#:target (make-consumer)))
 
-
-#;
-(render
- (make-link
-  #:source (make-producer #:type 'color #:source "green" #:start 0 #:end 800)
-  ;#:target (make-consumer #:type 'avformat #:target "output.mp4")))
-  #:target (make-consumer)))
-
+;; The demo video now scaled to a 16:10 ratio
 #;
 (render
  (make-link #:source (make-producer #:source demo)
             #:target (make-consumer #:prop (hash "width" (* 16 80)
                                                  "height" (* 10 80)))))
 
+;; Same as above, but now renders to output.mp4
 #;
 (render
- (make-link #:source (make-producer #:source "/Users/leif/demo.mkv")
+ (make-link #:source (make-producer #:source demo
             #:target (make-consumer #:type 'avformat
                                     #:target "output.mp4"
                                     #:prop (hash "width" (* 16 80)
-                                                 "height" (* 9 80)))))
+                                                 "height" (* 9 80))))))
 
+;; Displays a logo, and then shows a blue square
+;;   Also, render to XML
 #;
 (render
  (make-link
   #:source (make-playlist
-            #:producers (list (make-producer #:type #f #:source "hold:/Users/leif/logo.png"
+            #:producers (list (make-producer #:type #f #:source hold:logo
                                              #:start 0 #:end 200)
                               (make-producer #:type #f #:source "color:blue" #:start 0 #:end 200)))
-  #:target (make-consumer #:type 'avformat #:target "output.mp4")))
-  ;#:target (make-consumer)))
-  ;#:target (make-consumer #:type 'xml #:target "output.xml")))
+  #:target (make-consumer #:type 'xml #:target "output.xml")))
 
-#;
-(render
- (make-link
-  #:source
-  (make-tractor
-   #:multitrack (make-multitrack #:tracks (list (make-producer #:source demo
-                                                               #:prop (hash "width" 100
-                                                                            "height" 100))
-                                                (make-producer #:source demo
-                                                               #:prop (hash "width" 200
-                                                                            "height" 200)))))
-  #:target (make-consumer #:type 'xml)))
-
+;; Display a watermark over demo
 #;
 (render
  (make-link #:source (make-producer
