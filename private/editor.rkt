8101e770a1a4089b6d3f2568253563c4730bfae9
Leif Andersen <leif@leifandersen.net>
2016-12-15 14:40:07 -0500

    Initial GUI editor created.

---
+++ private/editor.rkt
@@ -0,0 +1,106 @@
+#lang racket/base
+
+(provide video-editor%)
+(require data/gvector
+         racket/class
+         racket/gui/base
+         racket/match
+         racket/list
+         pict
+         images/icons/style)
+
+(define video-editor%
+  (class pasteboard%
+    (init-field [track-height 100])
+    (super-new)
+    (define adjusting-clip? #f)
+    (match-define-values (track-width _) (send this get-max-view-size))
+    (define frames-per-pixel 1)
+    (define line-frequency 25)
+    (define line-number-frequency 4)
+    (define track-pict #f)
+    (define ruler-height 100)
+    
+    (define (update-track-pict!)
+      (set! track-pict
+            (bitmap-render-icon
+             (pict->bitmap (filled-rectangle track-width track-height
+                                             #:color "white"))
+             3/10)))
+    (update-track-pict!)
+    
+    (define (round-to-nearest-track n)
+      (let* ([acc (/ n track-height)]
+             [acc (round acc)]
+             [acc (* acc track-height)])
+        acc))
+
+    (define/augment (after-move-to s x y d)
+      (unless (or d adjusting-clip?)
+        (dynamic-wind
+         (λ () (set! adjusting-clip? #t))
+         (λ ()
+           (send this move-to s x (round-to-nearest-track y)))
+         (λ () (set! adjusting-clip? #f)))))
+
+    (define/override (on-paint before? dc left top right bottom dx dy draw-caret)
+      (when before?
+        (define-values (width height)
+          (send this get-max-view-size))
+        (unless (= width track-width)
+          (set! track-width width)
+          (update-track-pict!))
+        (for ([i (in-naturals)]
+              #:break (>= (+ (* i track-height) dy) height))
+          (send dc draw-bitmap track-pict 0 (+ (* i track-height) dy)))
+        (define possible-ruler-length
+          (- width dx))
+        (void))) ;; TODO: Draw frames
+
+    (define/augment (on-insert snip before x y)
+      (send snip resize 100 track-height))
+
+    (define/override (interactive-adjust-resize s w h)
+      (set-box! h track-height)
+      (super interactive-adjust-resize s w h))
+
+    (define (make-right-click-menu x y)
+      (define p (new popup-menu%))
+      (new menu-item%
+           [parent p]
+           [label "Insert Text"]
+           [callback (λ (item event)
+                       (define t (new text%))
+                       (send t set-max-undo-history 100)
+                       (send t insert "Hello World")
+                       (send this insert
+                             (make-object editor-snip% t)
+                             x
+                             (round-to-nearest-track y)))])
+      (new menu-item%
+           [parent p]
+           [label "Insert Graphical"]
+           [callback (λ (item event)
+                       (define pb (new video-editor%
+                                       [track-height (/ track-height 4)]))
+                       (send this insert
+                             (make-object editor-snip% pb)
+                             x
+                             (round-to-nearest-track y))
+                                              (define t (new text%))
+                       (send t set-max-undo-history 100)
+                       (send t insert "Hello World")
+
+                       (send pb insert (make-object editor-snip% t) 0 0)
+                       )])
+      p)
+
+    (define/override (on-default-event event)
+      (match (send event get-event-type)
+        ['right-down
+         (define admin (send this get-admin))
+         (define x (send event get-x))
+         (define y (send event get-y))
+         (when admin
+           (send admin popup-menu (make-right-click-menu x y) x y))]
+        [else (super on-default-event event)]))))
