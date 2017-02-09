75f2eb9fcf46c5f19c9e931147340a41c8d6e067
Leif Andersen <leif@leifandersen.net>
2017-02-09 15:49:13 -0500

    Fix playlist in main.rkt
    Since base.rkt defines its own playlist function, we should be
    using that in main.rkt, rather than the core one.

--- main.rkt
+++ main.rkt
@@ -5,7 +5,7 @@
          (all-from-out video/base))
 
 (require (prefix-in core: video/core)
-         (except-in video/core blank multitrack)
+         (except-in video/core blank multitrack playlist)
          video/base
          racket/list
          (except-in pict clip frame blank)
