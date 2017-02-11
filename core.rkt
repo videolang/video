4e982b5d34c430eb3d52671bba74f80f2fe4b926
Leif Andersen <leif@leifandersen.net>
2017-02-10 23:31:36 -0500

    Update core so that it only exports things that it _should_ export

--- core.rkt
+++ core.rkt
@@ -1,6 +1,41 @@
 #lang racket/base
 
-(require "private/video.rkt")
-(provide (except-out (all-from-out "private/video.rkt")
-                     subclass-empty
-                     define-constructor))
+(require racket/contract/base
+         racket/class
+         "render.rkt"
+         "private/video.rkt")
+(provide
+ (contract-out
+  [convert (->* [any/c]
+                [#:renderer (or/c (is-a? render%))]
+                any/c)])
+  make-video
+  video?
+  make-link
+  link?
+  make-properties
+  properties?
+  make-anim-property
+  anim-property?
+  make-frame
+  frame?
+  make-service
+  service?
+  make-filter
+  filter?
+  make-transition
+  transition?
+  make-consumer
+  consumer?
+  make-producer
+  producer?
+  make-blank
+  blank?
+  make-playlist
+  playlist?
+  make-playlist-producer
+  playlist-producer?
+  make-multitrack
+  multitrack?
+  make-field-element
+  field-element?)
