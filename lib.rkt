989cdc1ad9f7034687d0232072dd4454e5210281
Leif Andersen <leif@leifandersen.net>
2017-02-14 13:02:10 -0500

    Dropped playlist-append function.
    The playlist function works much better than playlist-append.
    
    Furthermore, producers should be quasi-atomic data structures. As such,
    if you want to append one to another, playlist *is* the operation you should be using.

--- lib.rkt
+++ lib.rkt
@@ -23,11 +23,7 @@
   [playlist-clip-length (-> any/c nonnegative-integer? nonnegative-integer?)]
   
   ;; Get the start time of a clip in a playlist
-  [playlist-clip-start (-> any/c nonnegative-integer? nonnegative-integer?)]
-
-  ;; Append two playlists together
-  ;; NOTE, any properties are currently dropped
-  [playlist-append (-> playlist? playlist? playlist?)]))
+  [playlist-clip-start (-> any/c nonnegative-integer? nonnegative-integer?)]))
 
 (define (producer-length producer)
   (define internal (convert producer))
@@ -43,7 +39,3 @@
 
 (define (playlist-clip-start playlist index)
   (mlt-playlist-clip-start (convert playlist)))
-
-(define (playlist-append pl1 pl2)
-  (make-playlist #:elements (append (playlist-elements pl1)
-                                    (playlist-elements pl2))))
