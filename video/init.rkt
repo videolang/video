a9a5b3cb71b7398b7b396462feea3c915ea46377
Leif Andersen <leif@leifandersen.net>
2017-03-03 11:53:25 -0500

    Since we can now load mlt multiple times in a process, no need to have the namespace functions.

--- video/init.rkt
+++ video/init.rkt
@@ -4,32 +4,4 @@
 ;; Alternatively your program can just call `mlt-factory-init` on its own.
 
 (require "private/init-mlt.rkt")
-(provide make-video-namespace
-         make-video-empty-namespace)
 
-(define-namespace-anchor anchor)
-
-(define (make-video-empty-namespace)
-  (let ([ns (make-base-empty-namespace)])
-    (namespace-attach-module (namespace-anchor->empty-namespace anchor)
-                             'video/init
-                             ns)
-    (namespace-attach-module (namespace-anchor->empty-namespace anchor)
-                             'racket/gui/base
-                             ns)
-    (namespace-attach-module (namespace-anchor->empty-namespace anchor)
-                             'video/core
-                             ns)
-    ns))
-
-(define (make-video-namespace)
-  (let ([ns (make-video-empty-namespace)])
-    (parameterize ([current-namespace ns])
-      (namespace-require 'racket/base)
-      (namespace-require 'racket/gui/base)
-      (namespace-require 'racket/class)
-      (namespace-require 'video/init)
-      (namespace-require 'video/render)
-      (namespace-require 'video/player)
-      (namespace-require 'video/core))
-    ns))
