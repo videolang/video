c7c2728cc93af49ce42800933c266c14d08966bc
Leif Andersen <leif@leifandersen.net>
2017-08-07 16:21:02 -0400

    Added Stephen Chang's name to files he touched.
    I should have done this a while back, but I am just getting to
    this now.

--- video/tests/unit-tests.rkt
+++ video/tests/unit-tests.rkt
@@ -1,7 +1,7 @@
 #lang video
 
 #|
-   Copyright 2016-2017 Leif Andersen
+   Copyright 2016-2017 Leif Andersen, Stephen Chang
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
