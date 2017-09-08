3b953cbd9804e401611485f966a393d8ea402e3c
Leif Andersen <leif@leifandersen.net>
2017-09-08 09:06:34 -0400

    Add (possible) linux support for input devs.

--- video/private/devices.rkt
+++ video/private/devices.rkt
@@ -29,41 +29,52 @@
          "ffmpeg-pipeline.rkt"
          "init.rkt")
 
-(struct device-list (video
-                     audio)
-  #:transparent)
-
-(define video-devices-str "AVFoundation video devices:")
-(define audio-devices-str "AVFoundation audio devices:")
-(define dev-regexp #rx"\\[[0-9]*\\] (.*)")
+(struct input-devices (video
+                       audio))
+(define (mk-input-devices #:video [v '()]
+                          #:audio [a '()])
+  (input-devices v a))
 
 (define (list-input-devices)
-  (define curr-list (box #f))
-  (define video-list (box '()))
-  (define audio-list (box '()))
+  (match (system-type 'os)
+    ['unix
+     (define fmt (av-find-input-format "avfoundation"))
+     (define ctx (avformat-alloc-context))
+     (avformat-open-input ctx "" fmt #f)
+     (avdevice-list-devices ctx)]
+    ['macosx
+     (define video-devices-str "AVFoundation video devices:")
+     (define audio-devices-str "AVFoundation audio devices:")
+     (define dev-regexp #rx"\\[[0-9]*\\] (.*)")
+
+     (define curr-list (box #f))
+     (define video-list (box '()))
+     (define audio-list (box '()))
   
-  (flush-ffmpeg-log!)
-  (with-intercepted-logging
-      (λ (l)
-        (match l
-          [(vector level message data topic)
-           (match message
-             [(regexp video-devices-str)
-              (set-box! curr-list video-list)]
-             [(regexp audio-devices-str)
-              (set-box! curr-list audio-list)]
-             [_
-              (define dev-name (cadr (regexp-match dev-regexp message)))
-              (set-box! (unbox curr-list)
-                        (cons dev-name (unbox (unbox curr-list))))])]))
-    (λ ()
-      (define fmt (av-find-input-format "avfoundation"))
-      (define ctx (avformat-alloc-context))
-      (with-handlers ([exn? (λ (e) (void))])
-        (avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true"))))
-      (flush-ffmpeg-log!))
-    #:logger ffmpeg-logger
-    'info
-    (string->symbol "AVFoundation input device"))
-  (device-list (reverse (unbox video-list))
-               (reverse (unbox audio-list))))
+     (flush-ffmpeg-log!)
+     (with-intercepted-logging
+         (λ (l)
+           (match l
+             [(vector level message data topic)
+              (match message
+                [(regexp video-devices-str)
+                 (set-box! curr-list video-list)]
+                [(regexp audio-devices-str)
+                 (set-box! curr-list audio-list)]
+                [_
+                 (define dev-name (cadr (regexp-match dev-regexp message)))
+                 (set-box! (unbox curr-list)
+                           (cons dev-name (unbox (unbox curr-list))))])]))
+       (λ ()
+         (define fmt (av-find-input-format "avfoundation"))
+         (define ctx (avformat-alloc-context))
+         (with-handlers ([exn? (λ (e) (void))])
+           (avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true"))))
+         (flush-ffmpeg-log!))
+       #:logger ffmpeg-logger
+       'info
+       (string->symbol "AVFoundation input device"))
+     (input-devices (reverse (unbox video-list))
+                    (reverse (unbox audio-list)))]
+    [_
+     (error "Not yet implemented for this platform")]))
