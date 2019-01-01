a191e2c7a7e22c7e5da2c7ffaaea90a43ebbf66f
Leif Andersen <leif@leifandersen.net>
2019-01-01 18:28:56 -0500

    Woops, negated error check twice.

--- video/private/ffmpeg/ffmpeg.rkt
+++ video/private/ffmpeg/ffmpeg.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 #|
-   Copyright 2016-2018 Leif Andersen
+   Copyright 2016-2019 Leif Andersen
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -466,7 +466,7 @@
             -> [ret : _int]
             -> (cond
                  [(= ret 0) out]
-                 [(= (- ret) AVERROR-EXIT)
+                 [(= ret AVERROR-EXIT)
                   (raise-ffmpeg-error 'open-input
                                       "Did not open input file:"
                                       'errcode ret
